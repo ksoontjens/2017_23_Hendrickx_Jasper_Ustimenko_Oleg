ba2a55830e19dcc4dcba02d40ff8a81df36213b4
jasperhen <jasper.hendrickx@hotmail.be>
2017-06-01 17:17:25 +0200

    initial commit

--- src/hellotvxlet/HelloTVXlet.java
+++ src/hellotvxlet/HelloTVXlet.java
@@ -1,55 +1,108 @@
 package hellotvxlet;
 
+
 import java.awt.Color;
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 import javax.tv.xlet.*;
+import org.bluray.ui.event.HRcEvent;
+import org.davic.resources.ResourceClient;
+import org.davic.resources.ResourceProxy;
+import org.dvb.event.EventManager;
+import org.dvb.event.UserEvent;
+import org.dvb.event.UserEventListener;
+import org.dvb.event.UserEventRepository;
+import org.havi.ui.HBackgroundConfigTemplate;
+import org.havi.ui.HBackgroundDevice;
+import org.havi.ui.HBackgroundImage;
+import org.havi.ui.HConfigurationException;
 import org.havi.ui.HContainer;
+import org.havi.ui.HPermissionDeniedException;
 import org.havi.ui.HScene;
 import org.havi.ui.HSceneFactory;
+import org.havi.ui.HScreen;
 import org.havi.ui.HStaticText;
+import org.havi.ui.HStillImageBackgroundConfiguration;
 import org.havi.ui.HTextButton;
 import org.havi.ui.HVisible;
 import org.havi.ui.event.HActionListener;
+import org.havi.ui.event.HBackgroundImageEvent;
+import org.havi.ui.event.HBackgroundImageListener;
 
 
-public class HelloTVXlet implements Xlet, HActionListener {
+
+public class HelloTVXlet implements Xlet, HActionListener, HBackgroundImageListener, ResourceClient
+// , UserEventListener
+
+{
+  HStaticText label2 ;
 HScene scene;
 
-HContainer cnt1 = new HContainer(0,0,500,500);
-HContainer cnt2 = new HContainer(0,0,500,500);
-HContainer cnt3 = new HContainer(0,0,500,500);
-HContainer cnt4 = new HContainer(0,0,500,500);
-HContainer cnt5 = new HContainer(0,0,500,500);
-HContainer cnt6 = new HContainer(0,0,500,500);
-HContainer cnt7 = new HContainer(0,0,500,500);
-HContainer cnt8 = new HContainer(0,0,500,500);
-HContainer cnt9 = new HContainer(0,0,500,500);
-HContainer cnt10 = new HContainer(0,0,500,500);
-HContainer cnt11 = new HContainer(0,0,500,500);
-HContainer cnt12 = new HContainer(0,0,500,500);
-HContainer cnt13 = new HContainer(0,0,500,500);
 
-HTextButton antw11 = new HTextButton("St.-Job", 30,300,200,100);
-HTextButton antw12 = new HTextButton("Edegem", 230,300,200,100); //1tru
-HTextButton antw13 = new HTextButton("Sumy", 30,400,200,100);  //2tru
-HTextButton antw14 = new HTextButton("Kiev", 230,400,200,100);
+HStaticText feedback;
+String vraag[]={"Waar is Jasper Hendrickx geboren?", "Hoe groot is Jasper?", "Hoeveel broers of zussen heeft Jasper", "Waar is Oleg Ustimenko geboren?", "Hoe groot is Oleg?", "Hoeveel broers of zussen heeft Oleg" };
+String ant[][]={{"St.-Job","Edegem","Sumy","Kiev"},{"1m83","2m","1m93","1m87"},{"0","1","2","onbekend"}, {"St.-Job","Edegem","Sumy","Kiev"},{"1m83","2m","1m93","1m87"},{"0","1","2","onbekend"} };
+int corr_ant[]={2,4,3,3,1,1};
+public UserEventRepository rep =new UserEventRepository("remove shit");
+public EventManager manager=EventManager.getInstance();
+public Timer t = new Timer();
+public int i = 0;
+int hvraag=0; // huidige vraag
+int toestand=State.WACHTOPANT;
+public TimerTask task = new TimerTask() {
+public void run(){
+    System.out.println("huidige toestand="+toestand+" i="+i);
+    int vtoestand=toestand;
+if (toestand==State.WACHTOPANT) //feedback tijd starten
+{
+    i=0;
+    //vtoestand=State.FEEDBACKTIMERGESTART;
+}
+    if (toestand==State.FEEDBACKTIMERGESTART)
+        if( i>=3)
+    {
+       
+        feedback.setVisible(false);
+        scene.repaint();
+        hvraag++;
+        System.out.println("hvraag="+hvraag);
+        
+        if (hvraag >=5) {
+            scene.repaint();
+            
+            feedback.setTextContent("uw score is"+ score, HVisible.NORMAL_STATE);
+            feedback.setBackgroundMode(HVisible.BACKGROUND_FILL);
+            feedback.setBackground(Color.GREEN);
+            scene.popToFront(feedback);
 
-HTextButton antw21 = new HTextButton("1m83", 30,300,200,100);
-HTextButton antw22 = new HTextButton("2m", 230,300,200,100);
-HTextButton antw23 = new HTextButton("1m93", 30,400,200,100);
-HTextButton antw24 = new HTextButton("1m87", 230,400,200,100);  //tru
+        }
+        else {
+        update_buttons();
+        
+        vtoestand=State.WACHTOPANT;
+        }
+    }
+        else i++;
 
-HTextButton antw31 = new HTextButton("0", 30,300,200,100); //2tru
-HTextButton antw32 = new HTextButton("1", 230,300,200,100);
-HTextButton antw33 = new HTextButton("2", 30,400,200,100);  //1tru
-HTextButton antw34 = new HTextButton("onbekend", 230,400,200,100);
+toestand=vtoestand;
 
-HTextButton antw41 = new HTextButton("inline hockey", 30,300,200,100);  //tru
-HTextButton antw42 = new HTextButton("rugby", 230,300,200,100);
-HTextButton antw43 = new HTextButton("voetbal", 30,400,200,100);
-HTextButton antw44 = new HTextButton("zwerkbal", 230,400,200,100);
+}
+};
+HContainer cnt;
+HStillImageBackgroundConfiguration hsbdc;
+
+
+HTextButton antw11 = new HTextButton(ant[hvraag][0], 30,300,200,100);
+HTextButton antw12 = new HTextButton(ant[hvraag][1], 230,300,200,100); //1tru
+HTextButton antw13 = new HTextButton(ant[hvraag][2], 30,400,200,100); //2tru
+HTextButton antw14 = new HTextButton(ant[hvraag][3], 230,400,200,100);
+
 
 public int score = 0;
+public String scoreString = "" + score;
 
 //jasper:
 //
@@ -67,223 +120,222 @@ public int score = 0;
 //2welke sport heeft oleg gedaan ooit? roller hockey, rugby, voetbal, zwerkbal
 //
 //3hoeveel broers of zussen heeft oleg? 2; 1, 0, onbekend
+public void update_buttons()
+{
+    System.out.println("updating buttons... hvraag="+hvraag);
+    label2.setTextContent(vraag[hvraag], HVisible.NORMAL_STATE);
+     antw11.setTextContent(ant[hvraag][0], HVisible.NORMAL_STATE);
+antw12.setTextContent(ant[hvraag][1], HVisible.NORMAL_STATE);
+     antw13.setTextContent(ant[hvraag][2], HVisible.NORMAL_STATE);
+antw14.setTextContent(ant[hvraag][3], HVisible.NORMAL_STATE);
+scene.repaint();
+}
+
+public void initXlet(XletContext context) {
+        try {
+
+            HScreen hs = HScreen.getDefaultHScreen();
+            HBackgroundDevice hbgd = hs.getDefaultHBackgroundDevice();
+
+            hbgd.reserveDevice(this);
+
+            HBackgroundConfigTemplate hbgdct = new HBackgroundConfigTemplate();
+            hbgdct.setPreference(HBackgroundConfigTemplate.STILL_IMAGE, HBackgroundConfigTemplate.REQUIRED);
 
 
+            hsbdc = (HStillImageBackgroundConfiguration) hbgd.getBestConfiguration(hbgdct);
 
-    public void initXlet(XletContext context) {
-      scene= HSceneFactory.getInstance().getDefaultHScene();
-      scene.add(cnt1);
-      
-      HStaticText label1 = new HStaticText("Kies een categorie", 30,30,400,100);
-      label1.setBackgroundMode(HVisible.BACKGROUND_FILL);
-      label1.setBackground(Color.LIGHT_GRAY);
-      HTextButton button1 = new HTextButton("Jasper", 30,300,200,100);
-      HTextButton button2 = new HTextButton("Oleg", 230,300,200,100);
-      
-      button1.setFocusTraversal(null, null, null, button2);
-      button2.setFocusTraversal(null, null, button1, null);
-      button1.setActionCommand("button1");
-      button2.setActionCommand("button2");
-      button1.addHActionListener(this);
-      button2.addHActionListener(this);
+            try {
 
-      HStaticText label2 = new HStaticText("Waar is Jasper Hendrickx geboren?", 30,30,400,100);
-      label2.setBackgroundMode(HVisible.BACKGROUND_FILL);
-      label2.setBackground(Color.LIGHT_GRAY);
-      
-      HStaticText label3 = new HStaticText("Hoe groot is Jasper?", 30,30,400,100);
-      label3.setBackgroundMode(HVisible.BACKGROUND_FILL);
-      label3.setBackground(Color.LIGHT_GRAY);
-      
-      HStaticText label4 = new HStaticText("Hoeveel broers of zussen heeft Jasper?", 30,30,400,100);
-      label4.setBackgroundMode(HVisible.BACKGROUND_FILL);
-      label4.setBackground(Color.LIGHT_GRAY);
-      
-      HStaticText label5 = new HStaticText("Waar is Oleg Ustimenko geboren?", 30,30,400,100);
-      label5.setBackgroundMode(HVisible.BACKGROUND_FILL);
-      label5.setBackground(Color.LIGHT_GRAY);
-      
-      HStaticText label6 = new HStaticText("Welke sport heeft Oleg gespeeld?", 30,30,400,100);
-      label6.setBackgroundMode(HVisible.BACKGROUND_FILL);
-      label6.setBackground(Color.LIGHT_GRAY);
-      
-      HStaticText label7 = new HStaticText("Hoeveel broers of zussen heeft Oleg?", 30,30,400,100);
-      label7.setBackgroundMode(HVisible.BACKGROUND_FILL);
-      label7.setBackground(Color.LIGHT_GRAY);
-      
+                hbgd.setBackgroundConfiguration(hsbdc);
 
-      
-      antw11.setFocusTraversal(null, antw13, null,  antw12);
-      antw12.setFocusTraversal(null,antw14, antw11, null);
-      antw13.setFocusTraversal(antw11, null, null, antw14);
-      antw14.setFocusTraversal(antw12, null , antw13, null);
-      
-      antw11.setActionCommand("antw11");
-      antw12.setActionCommand("antw12");
-      antw13.setActionCommand("antw13");
-      antw14.setActionCommand("antw14");
-      
-      antw11.addHActionListener(this);
-      antw12.addHActionListener(this);
-      antw13.addHActionListener(this);
-      antw14.addHActionListener(this);
-      
-      antw21.setFocusTraversal(null, antw23, null,  antw22);
-      antw22.setFocusTraversal(null,antw24, antw21, null);
-      antw23.setFocusTraversal(antw21, null, null, antw24);
-      antw24.setFocusTraversal(antw22, null , antw23, null);
-      
-      antw21.setActionCommand("antw21");
-      antw22.setActionCommand("antw22");
-      antw23.setActionCommand("antw23");
-      antw24.setActionCommand("antw24");
-      
-      antw21.addHActionListener(this);
-      antw22.addHActionListener(this);
-      antw23.addHActionListener(this);
-      antw24.addHActionListener(this);
-      
-      antw31.setFocusTraversal(null, antw33, null,  antw32);
-      antw32.setFocusTraversal(null,antw34, antw31, null);
-      antw33.setFocusTraversal(antw31, null, null, antw34);
-      antw34.setFocusTraversal(antw32, null , antw33, null);
-      
-      antw31.setActionCommand("antw31");
-      antw32.setActionCommand("antw32");
-      antw33.setActionCommand("antw33");
-      antw34.setActionCommand("antw34");
-      
-      antw31.addHActionListener(this);
-      antw32.addHActionListener(this);
-      antw33.addHActionListener(this);
-      antw34.addHActionListener(this);
-      
-      antw41.setFocusTraversal(null, antw43, null,  antw42);
-      antw42.setFocusTraversal(null,antw44, antw41, null);
-      antw43.setFocusTraversal(antw41, null, null, antw44);
-      antw44.setFocusTraversal(antw42, null , antw43, null);
-      
-      antw41.setActionCommand("antw41");
-      antw42.setActionCommand("antw42");
-      antw43.setActionCommand("antw43");
-      antw44.setActionCommand("antw44");
-      
-      antw41.addHActionListener(this);
-      antw42.addHActionListener(this);
-      antw43.addHActionListener(this);
-      antw44.addHActionListener(this);
-//      
-      
-      cnt1.add(label1);
-      cnt1.add(button1);
-      cnt1.add(button2);
-      
-      cnt2.add(label2);
-      cnt2.add(antw11);
-      cnt2.add(antw12);
-      cnt2.add(antw13);
-      cnt2.add(antw14);
-      
-      cnt3.add(label3);
-      cnt3.add(antw21);
-      cnt3.add(antw22);
-      cnt3.add(antw23);
-      cnt3.add(antw24);
-      
-      cnt4.add(label4);
-      cnt4.add(antw31);
-      cnt4.add(antw32);
-      cnt4.add(antw33);
-      cnt4.add(antw34);
-      
-//     zelfde buttons in een container gaat niet http://stackoverflow.com/questions/4620601/cant-a-swing-component-be-added-to-multiple-containers
-//      
-//      cnt5.add(label5);
-//      cnt5.add(antw11);
-//      cnt5.add(antw12);
-//      cnt5.add(antw13);
-//      cnt5.add(antw14);
-//      
-      cnt6.add(label6);
-      cnt6.add(antw41);
-      cnt6.add(antw42);
-      cnt6.add(antw43);
-      cnt6.add(antw44);
-//      
-//      cnt7.add(label7);
-//      cnt7.add(antw31);
-//      cnt7.add(antw32);
-//      cnt7.add(antw33);
-//      cnt7.add(antw34);
-      
-      scene.validate();
-      button1.requestFocus();
-      
-      scene.setVisible(true);
-    }
-
-    public void startXlet() {
-    
-    }
-
-    public void pauseXlet() {
-     
-    }
-
-    public void destroyXlet(boolean unconditional) {
-     
-    }
-
-    public void actionPerformed(ActionEvent arg0) {
-        System.out.println(arg0.getActionCommand());
-       
-            //hstatictext toevoegen met goed + toevoegen aan scene + scene repaint
-//            HStaticText feedback= new HStaticText ("goed", 150, 200, 400, 50);
-//            feedback.setBackgroundMode(HVisible.BACKGROUND_FILL);
-//            feedback.setBackground(Color.GREEN);
-//            scene.add(feedback);
-//            scene.popToFront(feedback);
-//            scene.repaint();
-            
-            scene.removeAll();
-          
-            cnt2.validate();
-            scene.add(cnt2);
-
-            scene.repaint();
-            antw11.requestFocus();
-           
-            System.out.println(arg0.getActionCommand());
-            if (arg0.getActionCommand().equals("antw12"))  //jasper
-                {
-            //hstatictext toevoegen met goed + toevoegen aan scene + scene repaint
-            //scene.removeAll();
-            HStaticText feedback= new HStaticText ("Juist! score +10", 150, 200, 400, 50);
-            feedback.setBackgroundMode(HVisible.BACKGROUND_FILL);
-            feedback.setBackground(Color.GREEN);
-            scene.add(feedback);
-            scene.popToFront(feedback);
-            scene.repaint();
-//            
-            //dit fucking werkt ni
-                }
-            
-            else {
-            //scene.removeAll();
-            //hstatictext toevoegen met slecht + toevoegen aan scene + scene repaint
-            HStaticText feedback= new HStaticText ("fout! score-5", 150, 200, 400, 50);
-            feedback.setBackgroundMode(HVisible.BACKGROUND_FILL);
-            feedback.setBackground(Color.RED);
-            scene.add(feedback);
-            scene.popToFront(feedback);
-            scene.repaint();
-            
-            scene.remove(cnt1);
-            scene.repaint();
+                //pizza uitpakken in die plek u kno
             }
 
+            catch (Exception ex) {
+
+                ex.printStackTrace();
+            }
+
+            HBackgroundImage img = new HBackgroundImage("bromans.jpg");
+            img.load(this);
+            hsbdc.displayImage(img);
+            cnt = new HContainer(0, 0, 500, 500);
+
+            scene = HSceneFactory.getInstance().getDefaultHScene();
+
+            scene.add(cnt);
+// rep.addAllArrowKeys();
+// manager.addUserEventListener(this, rep);
+            feedback = new HStaticText("feedback", 150, 200, 400, 50);
+            feedback.setBackgroundMode(HVisible.BACKGROUND_FILL);
+            feedback.setVisible(false);
+            t.scheduleAtFixedRate(task, 1000, 1000);
+            HStaticText label1 = new HStaticText("Kies een categorie", 30, 30, 400, 100);
+            label1.setBackgroundMode(HVisible.BACKGROUND_FILL);
+            label1.setBackground(Color.LIGHT_GRAY);
+            HTextButton button1 = new HTextButton("Jasper", 30, 300, 200, 100);
+            HTextButton button2 = new HTextButton("Oleg", 230, 300, 200, 100);
+
+            button1.setFocusTraversal(null, null, null, button2);
+            button2.setFocusTraversal(null, null, button1, null);
+            button1.setActionCommand("button1");
+            button2.setActionCommand("button2");
+            button1.addHActionListener(this);
+            button2.addHActionListener(this);
+
+            label2 = new HStaticText(vraag[hvraag], 30, 30, 400, 100);
+            label2.setBackgroundMode(HVisible.BACKGROUND_FILL);
+            label2.setBackground(Color.LIGHT_GRAY);
+
+
+            antw11.setFocusTraversal(null, antw13, null, antw12);
+            antw12.setFocusTraversal(null, antw14, antw11, null);
+            antw13.setFocusTraversal(antw11, null, null, antw14);
+            antw14.setFocusTraversal(antw12, null, antw13, null);
+
+
+
+            antw11.setActionCommand("antw11");
+            antw12.setActionCommand("antw12");
+            antw13.setActionCommand("antw13");
+            antw14.setActionCommand("antw14");
+
+            antw11.addHActionListener(this);
+            antw12.addHActionListener(this);
+            antw13.addHActionListener(this);
+            antw14.addHActionListener(this);
+
+
+            cnt.add(label2);
+            cnt.add(antw11);
+            cnt.add(antw12);
+            cnt.add(antw13);
+            cnt.add(antw14);
+
+            antw11.requestFocus();
+
+            scene.add(feedback);
+            scene.validate();
+            button1.requestFocus();
+
+            scene.setVisible(true);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } catch (HPermissionDeniedException ex) {
+            ex.printStackTrace();
+        } catch (HConfigurationException ex) {
+            ex.printStackTrace();
         }
+  
+}
+
+
+
+public void startXlet() {
+
+
+
+}
+
+
+
+
+
+
+public void pauseXlet() {
+  
+}
+
+public void destroyXlet(boolean unconditional) {
+ 
+}
+
+public void actionPerformed(ActionEvent arg0) {
+    System.out.println(arg0.getActionCommand());
+      
+if (toestand!=State.WACHTOPANT) return; // doe niks dan
+    
+    feedback.setVisible(true);
+        scene.repaint();
+toestand=State.FEEDBACKTIMERGESTART;
+        //cnt2.validate();
+
+        System.out.println(arg0.getActionCommand());
         
-       
+        String corrstr="antw1"+String.valueOf(corr_ant[hvraag]);
+        System.out.println("corrstr="+corrstr);
+        if (arg0.getActionCommand().equals(corrstr))  //jasper
+            {
+        //hstatictext toevoegen met goed + toevoegen aan scene + scene repaint
+        //scene.removeAll();
         
+        score +=10;
+        
+        System.out.println(score);
+        
+
+        feedback.setTextContent("Juist! score:" + score, HVisible.NORMAL_STATE);
+        feedback.setBackgroundMode(HVisible.BACKGROUND_FILL);
+        feedback.setBackground(Color.GREEN);
+
+        scene.popToFront(feedback);
+        
+      
+           // scene.removeAll();
+            scene.repaint();
     }
+        else
+
+                   {
+        //hstatictext toevoegen met goed + toevoegen aan scene + scene repaint
+        //scene.removeAll();
+        
+        score -=10;
+        
+        System.out.println(score);
+        
+
+        feedback.setTextContent("Fout! score:" + score, HVisible.NORMAL_STATE);
+        feedback.setBackgroundMode(HVisible.BACKGROUND_FILL);
+        feedback.setBackground(Color.RED);
+
+        scene.popToFront(feedback);
+        
+      
+           // scene.removeAll();
+            scene.repaint();
+    }
+}
+
+    public void imageLoaded(HBackgroundImageEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void imageLoadFailed(HBackgroundImageEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public boolean requestRelease(ResourceProxy proxy, Object requestData) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void release(ResourceProxy proxy) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    
+
+    public void notifyRelease(ResourceProxy proxy) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
+
+
+
+
+
+
+
+
+
---
+++ src/hellotvxlet/State.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hellotvxlet;
+
+/**
+ *
+ * @author student
+ */
+public class State {
+            
+    static int WACHTOPANT = 1;
+    static int FEEDBACKTIMERGESTART = 2;
+
+    
+}
